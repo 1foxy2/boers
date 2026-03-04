12b453673c9932d8d83409993bc5d9294a7cf134
1foxy2 <yarushas7@gmail.com>
2026-03-04 21:37:46 +0100

    Remove Config Enchantable Condition

--- src/main/java/net/foxy/boers/base/ModConditions.java
+++ src/main/java/net/foxy/boers/base/ModConditions.java
@@ -2,10 +2,7 @@ package net.foxy.boers.base;
 
 import com.mojang.serialization.MapCodec;
 import net.foxy.boers.BoersMod;
-import net.foxy.boers.data.ConfigEnchantableCondition;
-import net.neoforged.neoforge.common.conditions.AndCondition;
 import net.neoforged.neoforge.common.conditions.ICondition;
-import net.neoforged.neoforge.internal.versions.neoforge.NeoForgeVersion;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
